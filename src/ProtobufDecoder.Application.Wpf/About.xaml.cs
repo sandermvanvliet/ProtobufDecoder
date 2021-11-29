b08ed48e70ea55873212927eea4666e32130dd14
Sander van Vliet <sander@codenizer.nl>
2021-11-29 13:51:56 +0100

    Bootstrap WPF application

---
+++ src/ProtobufDecoder.Application.Wpf/About.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProtobufDecoder.Application.Wpf
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+
+            VersionTextBlock.Text = this.GetType().Assembly.GetName().Version.ToString();
+        }
+
+        private void OkButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ src/ProtobufDecoder.Application.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+
+namespace ProtobufDecoder.Application.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public DependencyProperty InputFilePathProperty = DependencyProperty.Register(
+            nameof(InputFilePath), 
+            typeof(string),
+            typeof(MainWindow));
+
+        public string InputFilePath
+        {
+            get => (string)GetValue(InputFilePathProperty);
+            set => SetValue(InputFilePathProperty, value);
+        }
+
+        private void AboutMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            var aboutWindow = new About
+            {
+                Owner = this,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+
+            aboutWindow.ShowDialog();
+        }
+
+        private void ExitMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BrowseFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                RestoreDirectory = true,
+                ShowReadOnly = true
+            };
+
+            var result = dialog.ShowDialog(this);
+
+            if (result.HasValue && result.Value)
+            {
+                InputFilePath = dialog.FileName;
+            }
+        }
+    }
+}
