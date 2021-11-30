5e393a966c6152bcd731652d60637fe98659778f
Sander van Vliet <sander@codenizer.nl>
2021-11-30 08:00:59 +0100

    Group properties in view based on Category attribute

--- src/ProtobufDecoder.Application.Wpf/ProtobufTagConverter.cs
+++ src/ProtobufDecoder.Application.Wpf/ProtobufTagConverter.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 
 namespace ProtobufDecoder.Application.Wpf
 {
-    [ValueConversion(typeof(ProtobufTag), typeof(List<KeyValuePair<string, string>>))]
+    [ValueConversion(typeof(ProtobufTag), typeof(List<ProtobufTagPropertyDescriptor>))]
     public class ProtobufTagConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -15,14 +16,28 @@ namespace ProtobufDecoder.Application.Wpf
             var tag = value as ProtobufTag;
 
             var properties = TypeDescriptor.GetProperties(tag);
-            var list = new List<KeyValuePair<string, string>>();
+            var list = new List<ProtobufTagPropertyDescriptor>();
 
             foreach (PropertyDescriptor p in properties)
             {
-                list.Add(new KeyValuePair<string, string>(p.Name, p.GetValue(tag)?.ToString()));
+                if(p.Attributes.OfType<BrowsableAttribute>().Any(a => a.Browsable == false))
+                {
+                    continue;
+                }
+
+                var category = GetCategoryOf(p);
+
+                list.Add(new ProtobufTagPropertyDescriptor(p.Name, p.GetValue(tag)?.ToString(), category));
             }
 
             return list;
+        }
+
+        private static string GetCategoryOf(PropertyDescriptor propertyDescriptor)
+        {
+            var category = propertyDescriptor.Attributes.OfType<CategoryAttribute>().SingleOrDefault();
+
+            return category?.Category;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
---
+++ src/ProtobufDecoder.Application.Wpf/ProtobufTagPropertyDescriptor.cs
@@ -0,0 +1,16 @@
+﻿namespace ProtobufDecoder.Application.Wpf
+{
+    public class ProtobufTagPropertyDescriptor
+    {
+        public ProtobufTagPropertyDescriptor(string name, string? value, string category)
+        {
+            Name = name;
+            Value = value;
+            Category = category;
+        }
+
+        public string Name { get; }
+        public string Value { get; }
+        public string Category { get; }
+    }
+}
