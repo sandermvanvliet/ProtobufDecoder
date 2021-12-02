b3bf169aa7ceefefd0cd4741253405e830e66dee
Sander van Vliet <sander@codenizer.nl>
2021-12-02 21:49:18 +0100

    Update app demo gif

--- src/ProtobufDecoder.Application.Wpf/App.xaml.cs
+++ src/ProtobufDecoder.Application.Wpf/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 using ProtobufDecoder.Application.Wpf.Models;
 using ProtobufDecoder.Application.Wpf.ViewModels;
@@ -12,6 +14,14 @@ namespace ProtobufDecoder.Application.Wpf
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
+#if SCREEN_RECORDING
+            var menuDropAlignmentField = typeof(SystemParameters).GetField("_menuDropAlignment", BindingFlags.NonPublic | BindingFlags.Static);
+            Action setAlignmentValue = () => {
+                if (SystemParameters.MenuDropAlignment && menuDropAlignmentField != null) menuDropAlignmentField.SetValue(null, false);};
+            setAlignmentValue();
+            SystemParameters.StaticPropertyChanged += (sender, e) => { setAlignmentValue(); };
+#endif
+
             var viewModel = new MainWindowViewModel
             {
                 Model = new MainWindowModel()
